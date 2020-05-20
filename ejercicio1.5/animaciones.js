552875d08f02e64ebefcbf12ebbf665f9460f4cf
Facundo Ferrara <facuferrarahotmail.com>
2020-05-20 15:02:24 -0300

    ej 5

--- ejercicio1.5/animaciones.js
+++ ejercicio1.5/animaciones.js
@@ -4,6 +4,9 @@ function cambiarDimensionFuente(size) {
     document.body.style.fontSize = size + 'px';
   };
 }
+
+
+
 var size8 = cambiarDimensionFuente(8);
 //2 aqui se cambia el style y dimencion de la letra a 8 gracias al callback de la funtion anterior
 var size16 = cambiarDimensionFuente(16);
@@ -11,6 +14,7 @@ var size24 = cambiarDimensionFuente(24);
 
  function setClicks(){
   //3.comentando esta funcion nos dice que hay un error de referencia ya que el callback no esta capturando nada
+  document.size8
   document.getElementById('fuente-8').onclick = size8;
   //2. y aqui luego es utilizado para setearlo con el click .
   //4. Debemos escribirlo de esta forma document.getElementById('fuente-8').onclick = size8; de lo contrario pensara que size en una function y no una variable (callback)  y no funcionara
