07b72a4f8a8d17a98a5c60a272a3b9fc0d878b31
Facundo Ferrara <facuferrarahotmail.com>
2020-05-20 23:31:48 -0300

    Update sinClosures.js

--- ejercicio1.5/sinClosures.js
+++ ejercicio1.5/sinClosures.js
@@ -1,25 +1,32 @@
-// var size8 = cambiarDimensionFuente(8);
-// var size16 = cambiarDimensionFuente(16);
-// var size24 = cambiarDimensionFuente(24);
 
 
 
-function cambiarDimensionFuente(size) {
-  document.getElementById('fuente-8').onclick = document.body.style.fontSize = size + 'px';
-    return document.body.style.fontSize = size + '8'
+function cambiarDimensionFuente(size) { // crea
+  //muestra
+  var dim8 = (document.body.style.fontSize = size + 'px');
+    return dim8;
+    
   // return function() { 
-  //   document.body.style.fontSize = size + 'px';
+    //   document.body.style.fontSize = size + 'px';
   };
-// }
+  // }
+  
+  function 2doproce(){
+    
+  }
+  
+  function setClicks(){
+    // document.getElementById('fuente-8').onclick = size8(); 
+    // document.getElementById('fuente-8').onclick = size8; 
+    // document.getElementById('fuente-16').onclick = size16;
+    // document.getElementById('fuente-24').onclick = size24;
+    
+    var size8 = document.getElementById('fuente-8').onclick = cambiarDimensionFuente(8);
+    document.getElementById('fuente-16').onclick= cambiarDimensionFuente(16);
+    document.getElementById('fuente-24').onclick= cambiarDimensionFuente(24);
+  }
 
-
-function setClicks(){
-  // document.getElementById('fuente-8').onclick = size8(); 
-  // document.getElementById('fuente-8').onclick = size8; 
-  // document.getElementById('fuente-16').onclick = size16;
-  // document.getElementById('fuente-24').onclick = size24;
-
-  document.getElementById('fuente-8').onclick;
-  document.getElementById('fuente-16').onclick;
-  document.getElementById('fuente-24').onclick;
-}
+  //el programa arranca asi
+//var size8 = cambiarDimensionFuente(8);  //variable que inicializa la primera funcion y la ejecuta
+// var size16 =  cambiarDimensionFuente(16); 
+// var size24 = cambiarDimensionFuente(24);
